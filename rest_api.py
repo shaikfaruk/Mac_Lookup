c6938e667c0909082d4faaa9cf39eeb5a6fa0626
shaikfaruk <35495397+shaikfaruk@users.noreply.github.com>
2020-11-26 11:00:43 +0530

    Update rest_api.py

--- rest_api.py
+++ rest_api.py
@@ -1,17 +1,23 @@
 import argparse
 import re
 import requests
+from configparser import ConfigParser
 import sys
 import json
+#Access the api_key from config.ini file.
+config = ConfigParser()
+config.read('config.ini')
+default_api_key = config.get('api_key','key')
+#Getting input from command line.
 parser = argparse.ArgumentParser()
-
-parser.add_argument("-mac", type=str, action="store", dest="mac", help="Enter a valid MAC address")
-parser.add_argument("-apikey", type=str, action="store", dest="apikey", default='at_5KCCwxRxbucCOKt2M4WyLX9KRzAdK', help="Enter a valid API Key")
-
+parser.add_argument("-mac", type=str, action="store", dest="mac", help="Enter a valid MAC address") 
+parser.add_argument("-apikey", type=str, action="store", dest="apikey", default=default_api_key, help="Please provide a valid API Key")
+#Stores MAC address and ApiKey.
 results = parser.parse_args()
-mac = results.mac
+mac = results.mac  
 api_key = results.apikey
 url = "https://api.macaddress.io/v1?apiKey="+api_key+"&output=json&search="
+#To check whether provided MAC address is of proper format or not.
 regex = re.search("^[a-fA-F0-9:]{17}|[a-fA-F0-9]{12}$", mac)
 if (regex):
     url = url+mac
@@ -20,6 +26,7 @@ else:
     sys.exit()  
 payload = {}
 headers= {}
+#Error handling for handling different types of errors
 try:
     response = requests.get( url, headers=headers, data = payload,timeout = 4)    
     response.raise_for_status()
@@ -35,6 +42,7 @@ except requests.exceptions.Timeout:
 except requests.exceptions.RequestException as err:
     print ("OOps: Something Else : ",err)
     sys.exit()
+#Checks whether the entered MAC address is there in database or not.
 res = json.loads(response.text.encode('utf8'))
 if (res["vendorDetails"]["companyName"] == ""):
     print("Sorry can't find the commany name, Please check the MAC address..!")
